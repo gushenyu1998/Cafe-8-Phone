daacc0f1b34dd799286137c1c6ea21a38d4e9743
gushenyu1998 <71572428+gushenyu1998@users.noreply.github.com>
2024-09-15 17:28:42 -0700

    Add internet permission for build

--- Components/ConnectionStatus.tsx
+++ Components/ConnectionStatus.tsx
@@ -4,17 +4,23 @@ import {View, StyleSheet, Alert, Text} from "react-native";
 type ConnectionStatus = {
     success: boolean;
 }
-
+const netConfig = require("../Config/Network.json");
+const hostname = netConfig.protocol + "//" + netConfig.address + ":" + netConfig.port;
 export default function ConnectionStatus({success}: ConnectionStatus) {
     return (
-        <View style={{...styles.container, backgroundColor:success?"green":"#d8c36b"}}>
-            <Text style={{fontSize: 12.5, fontWeight: 'bold', textAlign:'center', color:"white"}}>{success ? "Connected" : "Connecting"}</Text>
+        <View style={{...styles.container, backgroundColor: success ? "green" : "#d8c36b"}}>
+            <Text style={{
+                fontSize: 12.5,
+                fontWeight: 'bold',
+                textAlign: 'center',
+                color: "white"
+            }}>{success ? "Connected" : "Connecting to: "+hostname}</Text>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        justifyContent:'center',
+        justifyContent: 'center',
     }
 })
